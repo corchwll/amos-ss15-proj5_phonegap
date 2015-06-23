c9c08cb9b0e2eb5e229b6f02c1aacdf6dd92babd
Matthias Altstadt <matthias.altstadt@fau.de>
2015-06-24 01:07:59 +0200

    added project update functionality to the controller file

--- src/js/controllers/addProject.js
+++ src/js/controllers/addProject.js
@@ -1,6 +1,13 @@
 angular.module('MobileTimeRecording.controllers.AddProject', ['MobileTimeRecording.services.Database'])
 
-.controller('AddProjectController', function($scope, Projects, $location, ngNotify, $timeout){
+.controller('AddProjectController', function($scope, Projects, $location, ngNotify, $timeout, $routeParams){
+
+	$scope.getProject = function() {
+		Projects.getById($routeParams.projectId).then(function(project) {
+			$scope.project = project;
+			$scope.origDate = project.timestamp_final_date*1000;
+		});
+	};
 	
 	/**
 	 * This function adds a new project into the database, gives a respective notification and forwards to the main screen.
@@ -21,6 +28,27 @@ angular.module('MobileTimeRecording.controllers.AddProject', ['MobileTimeRecordi
 	  		$(location).attr('href', '#/');
 	  	}, 3500);
 	  });
+  };
+
+  /**
+   * This function updates a specified project, gives a respective notification and forwards to the main screen.
+   * 
+   * @param  project Project object containing the data of the edited project
+   */
+  $scope.editProject = function(project) {
+  	// convert date to unix timestamp
+		project.date = Date.parse(project.date)/1000;
+
+		Projects.update(project, project).then(function() {
+			ngNotify.set(project.name + ' successfully edited', {
+					type: 'success',
+					position: 'top',
+					duration: 3000
+				});
+			$timeout(function() {
+				$(location).attr('href', '#/');
+			}, 3500);
+		});
   };
 
   /**
