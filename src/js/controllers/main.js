034eaa613199496fc29c6372627d5d5118f2cbcd
Matthias Altstadt <matthias.altstadt@fau.de>
2015-06-23 22:49:32 +0200

    added functionality to sort the project list by gps distance

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,37 +1,82 @@
 angular.module('MobileTimeRecording.controllers.Main', ['MobileTimeRecording.services.Database'])
 
-.controller('MainController', function($scope, Projects){
+.controller('MainController', function($scope, Projects, User, $timeout){
   $scope.projects = [];
   $scope.project = null;
+
+  var gpsSort = 0;
+  var currentPosition = {};
   
   /**
-   * This function refreshes the view of the project lists and, therefore, loads all projects from the database.
+   * This function refreshes the view of the project lists and, therefore, loads all projects from the database and sorts them.
    * 
    */
   $scope.updateProjects = function() {
   	Projects.all().then(function(projects) {
-      $scope.delete = false;
-  		$scope.projects = projects;
-  	});
-  };
+      User.all().then(function(user) {
+        gpsSort = user[0].location_sort_is_used;
+        $scope.delete = false;
 
-  /**
-   * This function is used to arrange the projects in alphabetical order them with exception of the four predefined projects which are listed on top.
-   * 
-   * @param   project An object containing a project
-   * @return          The project name
-   */
-  $scope.orderProjects = function(project) {
-    if(project.id === '00001') {
-      return -1;
-    } else if(project.id === '00002') {
-      return -1;
-    } else if(project.id === '00003') {
-      return -1;
-    } else if(project.id === '00004') {
-      return -1;
-    } 
-    return project.name;
+        if(gpsSort === 1) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            $timeout(function() {
+              currentPosition.latitude = position.coords.latitude;
+              currentPosition.longitude = position.coords.longitude;
+              projects.sort(function(a, b) {
+                var distA, distB;
+                if(a.latitude === 'undefined' && a.longitude === 'undefined') {
+                  distA = Number.MAX_VALUE;
+                } else {
+                  distA = calculateDistance(currentPosition, a);
+                }
+                if(b.latitude === 'undefined' && b.longitude === 'undefined') {
+                  distB = Number.MAX_VALUE;
+                } else {
+                  distB = calculateDistance(currentPosition, b);
+                }
+
+                if(a.id === '00001') {
+                  return -1;
+                } else if(a.id === '00002') {
+                  return -1;
+                } else if(a.id === '00003') {
+                  return -1;
+                } else if(a.id === '00004') {
+                  return -1;
+                } else if(distA === distB) {
+                  return 0;
+                } else if(distA < distB) {
+                  return -1;
+                } else if(distA > distB) {
+                  return 1;
+                }
+              });
+              $scope.projects = projects;
+            }, 1);
+            
+          });
+        } else {
+          projects.sort(function(a, b) {
+            if(a.id === '00001') {
+              return -1;
+            } else if(a.id === '00002') {
+              return -1;
+            } else if(a.id === '00003') {
+              return -1;
+            } else if(a.id === '00004') {
+              return -1;
+            } else if(a.name === b.name) {
+              return 0;
+            } else if(a.name < b.name) {
+              return -1;
+            } else if(a.name > b.name) {
+              return 1;
+            }
+          });
+          $scope.projects = projects;
+        }
+      });
+  	});
   };
 
   /**
@@ -60,5 +105,27 @@ angular.module('MobileTimeRecording.controllers.Main', ['MobileTimeRecording.ser
     Projects.archive(project.id).then(function() {
       $scope.updateProjects();
     });
+  };
+
+  /**
+   * This function calculates the distance between the two positions.
+   * The algorithm is based on: http://stackoverflow.com/questions/365826/calculate-distance-between-2-gps-coordinates?answertab=active#tab-top
+   * 
+   * @param   currentPosition An Object containing latitude and longitude
+   * @param   project         An Object containing latitude and longitude
+   * @return                  The distance between the two postitons
+   */
+  var calculateDistance = function(currentPosition, project) {
+     var lat1 = currentPosition.latitude;
+     var lon1 = currentPosition.longitude;
+     var lat2 = project.latitude;
+     var lon2 = project.longitude;
+
+     var R = 6371; // Radius of the earth in km
+     var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+     var dLon = (lon2 - lon1) * Math.PI / 180;
+     var a = 0.5 - Math.cos(dLat)/2 + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * (1 - Math.cos(dLon))/2;
+
+     return R * 2 * Math.asin(Math.sqrt(a));
   };
 });
