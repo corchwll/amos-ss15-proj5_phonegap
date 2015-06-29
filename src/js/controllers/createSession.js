87fe64fe55087d56aa4094b14d565dc88a2b28d8
Matthias Altstadt <matthias.altstadt@fau.de>
2015-06-29 19:01:16 +0200

    added function to check if a final project date has already been reached and implemented it to notify the user if a session is added after the final project date

--- src/js/controllers/createSession.js
+++ src/js/controllers/createSession.js
@@ -17,7 +17,6 @@ angular.module('MobileTimeAccounting.controllers.CreateSession', ['MobileTimeAcc
 		session.timestamp_stop = Date.parse(session.date + ", " + session.stopTime)/1000;
 		var day = moment(session.date).format("YYYY-MM-DD");
 
-
 		if(!session.date) {
 			ngNotify.set('Please enter a date', {
 				type: 'error',
@@ -34,28 +33,40 @@ angular.module('MobileTimeAccounting.controllers.CreateSession', ['MobileTimeAcc
 						duration: 3000
 					});
 				} else {
-					Sessions.checkFullOverlapping(session.timestamp_start, session.timestamp_stop).then(function(result) {
-						if(result.overlappings === 0) {
-							Sessions.add(session).then(function() {
-								if(day === moment().format("YYYY-MM-DD")) {
-									cordova.plugins.notification.local.cancel(1, function() {
-									  console.log("Daily notification canceled for today");
-									});
-								}
-						  	ngNotify.set('Session successfully added', {
-					  			type: 'success',
-					  			position: 'top',
-					  			duration: 3000
-					  		});
-						  	$timeout(function() {
-						  		$(location).attr('href', '#/viewProject/' + session.project_id);
-						  	}, 4000);
-					  	});
-						} else {
-							ngNotify.set('You have already recorded for this time', {
+					/* Check for project end date */
+					Projects.getById(session.project_id).then(function(project) {
+						var finalDate = project.timestamp_final_date;
+						if(projectExpired(session.timestamp_start, finalDate)) {
+							ngNotify.set('It is not possible to record times after the final project date', {
 								type: 'error',
 								position: 'top',
 								duration: 3000
+							});
+						} else {
+							Sessions.checkFullOverlapping(session.timestamp_start, session.timestamp_stop).then(function(result) {
+								if(result.overlappings === 0) {
+									Sessions.add(session).then(function() {
+										if(day === moment().format("YYYY-MM-DD")) {
+											cordova.plugins.notification.local.cancel(1, function() {
+											  console.log("Daily notification canceled for today");
+											});
+										}
+								  	ngNotify.set('Session successfully added', {
+							  			type: 'success',
+							  			position: 'top',
+							  			duration: 3000
+							  		});
+								  	$timeout(function() {
+								  		$(location).attr('href', '#/viewProject/' + session.project_id);
+								  	}, 4000);
+							  	});
+								} else {
+									ngNotify.set('You have already recorded for this time', {
+										type: 'error',
+										position: 'top',
+										duration: 3000
+									});
+								}
 							});
 						}
 					});
@@ -69,5 +80,22 @@ angular.module('MobileTimeAccounting.controllers.CreateSession', ['MobileTimeAcc
 				duration: 3000
 			});
 	  }
+  };
+
+  /**
+   * This function checks if the final project date has been reached already.
+   * 
+   * @param  currentDate Unix timestamp
+   * @param  finalDate   Unix timestamp
+   * @return             Boolean
+   */
+  var projectExpired = function(currentDate ,finalDate) {
+  	if(!finalDate) {
+  		return false;
+  	} else if(currentDate >= finalDate) {
+  		return true;
+  	} else {
+  		return false;
+  	}
   };
 });
