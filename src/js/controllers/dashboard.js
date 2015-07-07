c4bb1d3c1e150bdbf4444bdd7d406fe4e860ac72
Matthias Altstadt <matthias.altstadt@fau.de>
2015-07-07 17:12:41 +0200

    added holiday service to createSession and dashboard file and removed thereby unnecessary functions

--- src/js/controllers/dashboard.js
+++ src/js/controllers/dashboard.js
@@ -1,6 +1,6 @@
-angular.module('MobileTimeAccounting.controllers.Dashboard', ['MobileTimeAccounting.services.Database'])
+angular.module('MobileTimeAccounting.controllers.Dashboard', ['MobileTimeAccounting.services.Database', 'MobileTimeAccounting.services.Holidays'])
 
-.controller('DashboardController', function($scope, Sessions, User, Projects, DummyMonth, $q, $timeout){
+.controller('DashboardController', function($scope, Sessions, User, Projects, DummyMonth, $q, $timeout, Holidays){
 
 	$scope.showVacationInfo = false;
 
@@ -206,223 +206,27 @@ angular.module('MobileTimeAccounting.controllers.Dashboard', ['MobileTimeAccount
 		var amountOfHolidays = 0;
 
 		if(moment(startDate).format("YYYY") === moment(stopDate).format("YYYY")) {
-			var holidays = getHolidaysForYear(moment(startDate).format("YYYY"));
-			amountOfHolidays += amountOfHolidaysBetween(holidays, startDate, stopDate);
+			var holidays = Holidays.getHolidaysForYear(moment(startDate).format("YYYY"));
+			amountOfHolidays += Holidays.amountOfHolidaysBetween(holidays, startDate, stopDate);
 		} else if(moment(stopDate).format("YYYY") - moment(startDate).format("YYYY") === 1) {
-			var holidaysInStartYear1 = getHolidaysForYear(moment(startDate).format("YYYY"));
-			var holidaysInStopYear1 = getHolidaysForYear(moment(stopDate).format("YYYY"));
+			var holidaysInStartYear1 = Holidays.getHolidaysForYear(moment(startDate).format("YYYY"));
+			var holidaysInStopYear1 = Holidays.getHolidaysForYear(moment(stopDate).format("YYYY"));
 
-			amountOfHolidays += amountOfHolidaysSince(holidaysInStartYear1, startDate);
-			amountOfHolidays += amountOfHolidaysUntil(holidaysInStopYear1, stopDate);
+			amountOfHolidays += Holidays.amountOfHolidaysSince(holidaysInStartYear1, startDate);
+			amountOfHolidays += Holidays.amountOfHolidaysUntil(holidaysInStopYear1, stopDate);
 		} else if(moment(stopDate).format("YYYY") - moment(startDate).format("YYYY") > 1) {
-			var holidaysInStartYear2 = getHolidaysForYear(moment(startDate).format("YYYY"));
-			var holidaysInStopYear2 = getHolidaysForYear(moment(stopDate).format("YYYY"));
+			var holidaysInStartYear2 = Holidays.getHolidaysForYear(moment(startDate).format("YYYY"));
+			var holidaysInStopYear2 = Holidays.getHolidaysForYear(moment(stopDate).format("YYYY"));
 
-			amountOfHolidays += amountOfHolidaysSince(holidaysInStartYear2, startDate);
-			amountOfHolidays += amountOfHolidaysUntil(holidaysInStopYear2, stopDate);
+			amountOfHolidays += Holidays.amountOfHolidaysSince(holidaysInStartYear2, startDate);
+			amountOfHolidays += Holidays.amountOfHolidaysUntil(holidaysInStopYear2, stopDate);
 
 			for(var i = moment(startDate).format("YYYY") + 1; i < moment(stopDate).format("YYYY"); i++) {
-				amountOfHolidays += getHolidaysForYear(i).length;
+				amountOfHolidays += Holidays.getHolidaysForYear(i).length;
 			}
 		}
 
 		return amountOfHolidays;
-	};
-
-	/**
-	 * This function computes the dates of holidays in a given year
-	 * 
-	 * @param   year The year of which the holidays should be computed
-	 * @return       An Array containing the holidays of one year
-	 */
-	var getHolidaysForYear = function(year) {
-		var holidays = getFixedHolidays(year);
-		var easterSunday = getEastern(year);
-
-		//Karfreitag
-		easterSunday = moment(easterSunday).subtract(2, 'days');
-		holidays.push(easterSunday);
-
-		//Ostermontag
-		easterSunday = moment(easterSunday).add(3, 'days');
-		holidays.push(easterSunday);
-
-		//Christi Himmelfahrt
-		easterSunday = moment(easterSunday).add(38, 'days');
-		holidays.push(easterSunday);
-
-		//Pfingstmontag
-		easterSunday = moment(easterSunday).add(11, 'days');
-		holidays.push(easterSunday);
-
-		//Fronleichnam
-		easterSunday = moment(easterSunday).add(10, 'days');
-		holidays.push(easterSunday);
-
-		return holidays;
-	};
-
-	/**
-	 * This function returns the dates of holidays with fixed day and month in a given year.
-	 * 
-	 * @param   year The year of which the holidays should be returned
-	 * @return       An Array containing the fixed holidays of one year
-	 */
-	var getFixedHolidays = function(year) {
-		var fixedHolidays = [];
-		var tmpDate;
-		//Neujahrstag
-		tmpDate = moment().set({'year': year, 'month': 0, 'date': 1});
-		if(!isSaturday(tmpDate) && !isSunday(tmpDate)) {
-			fixedHolidays.push(tmpDate);
-		}
-		//Heilige Drei Koenige
-		tmpDate = moment().set({'year': year, 'month': 0, 'date': 6});
-		if(!isSaturday(tmpDate) && !isSunday(tmpDate)) {
-			fixedHolidays.push(tmpDate);
-		}
-		//Tag der Arbeit
-		tmpDate = moment().set({'year': year, 'month': 4, 'date': 1});
-		if(!isSaturday(tmpDate) && !isSunday(tmpDate)) {
-			fixedHolidays.push(tmpDate);
-		}
-		//Maria Himmelfahrt
-		tmpDate = moment().set({'year': year, 'month': 7, 'date': 15});
-		if(!isSaturday(tmpDate) && !isSunday(tmpDate)) {
-			fixedHolidays.push(tmpDate);
-		}
-		//Tag der deutschen Einheit
-		tmpDate = moment().set({'year': year, 'month': 9, 'date': 3});
-		if(!isSaturday(tmpDate) && !isSunday(tmpDate)) {
-			fixedHolidays.push(tmpDate);
-		}
-		//Allerheiligen
-		tmpDate = moment().set({'year': year, 'month': 10, 'date': 1});
-		if(!isSaturday(tmpDate) && !isSunday(tmpDate)) {
-			fixedHolidays.push(tmpDate);
-		}
-		//1. Weihnachtstag
-		tmpDate = moment().set({'year': year, 'month': 11, 'date': 25});
-		if(!isSaturday(tmpDate) && !isSunday(tmpDate)) {
-			fixedHolidays.push(tmpDate);
-		}
-		//2. Weihnachtstag
-		tmpDate = moment().set({'year': year, 'month': 11, 'date': 26});
-		if(!isSaturday(tmpDate) && !isSunday(tmpDate)) {
-			fixedHolidays.push(tmpDate);
-		}
-
-		return fixedHolidays;
-	};
-
-	/**
-	 * This function checks if a given date is a saturday.
-	 * 
-	 * @param    date The date to check
-	 * @return        A boolean
-	 */
-	var isSaturday = function(date) {
-		if(moment(date).format("dddd") === 'Saturday') {
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	/**
-	 * This function checks if a given date is a sunday.
-	 * 
-	 * @param    date The date to check
-	 * @return        A boolean
-	 */
-	var isSunday = function(date) {
-		if(moment(date).format("dddd") === 'Sunday') {
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	/**
-	 * This function calculates the date of the easter sunday in a given year.
-	 * 
-	 * @param   year The year of which the easter sunday should be calculated
-	 * @return       Date of easter sunday
-	 */
-	var getEastern = function(year) {
-		var a = Math.floor(year % 19),
-	      b = Math.floor(year / 100),
-	      c = Math.floor(year % 100),
-	      d = Math.floor(b / 4),
-	      e = Math.floor(b % 4),
-	      f = Math.floor((b + 8) / 25),
-	      g = Math.floor((b - f + 1) / 3),
-	      h = Math.floor((19 * a + b - d -g + 15) % 30),
-	      i = Math.floor(c / 4),
-	      k = Math.floor(c % 4),
-	      l = Math.floor((32 + 2 * e + 2 * i - h - k) % 7),
-	      m = Math.floor((a + 11 * h + 22 * l) / 451),
-	      n = Math.floor((h + l - 7 * m + 114) / 31),
-	      p = Math.floor((h + l - 7 * m + 114) % 31);
-	  return moment({year: year, month: n - 1, day: p + 1});
-	};
-
-	/**
-	 * This function computes the amount of holidays in a given timeframe
-	 * 
-	 * @param   holidays  An object containing the dates of all holidays of a year
-	 * @param   startDate The beginning of the timeframe
-	 * @param   stopDate  The end of the timeframe
-	 * @return            The number of holidays
-	 */
-	var amountOfHolidaysBetween = function(holidays, startDate, stopDate) {
-		var result = 0;
-
-		angular.forEach(holidays, function(holiday) {
-			if(moment(startDate).isBefore(holiday) && moment(stopDate).isAfter(holiday)) {
-				result++;
-			}
-		});
-
-		return result;
-	};
-
-	/**
-	 * This function computes the amount of holidays from a certain date until the end of the respective year
-	 * 
-	 * @param   holidays  An object containing the dates of all holidays of a year
-	 * @param   startDate The beginning of the timeframe
-	 * @return            The number of holidays
-	 */
-	var amountOfHolidaysSince = function(holidays, startDate) {
-		var result = 0;
-
-		for(var i = 0; i < holidays.length; i++) {
-			if(moment(startDate).isBefore(holidays[i])) {
-				result++;
-			}
-		}
-
-		return result;
-	};
-
-	/**
-	 * This function computes the amount of holidays from the beginning of a year until a given date in that year
-	 * 
-	 * @param   holidays  An object containing the dates of all holidays of a year
-	 * @param   stopDate  The end of the timeframe
-	 * @return            The number of holidays
-	 */
-	var amountOfHolidaysUntil = function(holidays, stopDate) {
-		var result = 0;
-
-		for(var i = 0; i < holidays.length; i++) {
-			if(moment(holidays[i]).isBefore(stopDate)) {
-				result++;
-			}
-		}
-
-		return result;
 	};
 
 	/**
